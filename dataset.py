0d70876acc0767ed30ac3643bda3c45ca1692117
Andrew Katsipanos <andrewkatsipanos@kubrickgroup.com>
2023-07-05 16:53:50 +0100

    training updates

--- dataset.py
+++ dataset.py
@@ -30,6 +30,7 @@ class FruitDataset(torch.utils.data.Dataset):
 
         b = xmltodict.parse(a)['annotation']
 
+        num_objs = 0
         if type(b['object']) is list:
             for i in b['object']:
                 boxes.append([int(i['bndbox']['xmin']),
@@ -37,20 +38,24 @@ class FruitDataset(torch.utils.data.Dataset):
                               int(i['bndbox']['xmax']), 
                               int(i['bndbox']['ymax'])])
                 labels.append(label_dict[i['name']])
-                
+                num_objs+=1
+
         else:
             boxes.append([int(b['object']['bndbox']['xmin']),
                           int(b['object']['bndbox']['ymin']),
                           int(b['object']['bndbox']['xmax']), 
                           int(b['object']['bndbox']['ymax'])])
             labels.append(label_dict[b['object']['name']])
+            num_objs = 1
 
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
         target['boxes'] = boxes
         target['labels'] = torch.as_tensor(labels, dtype=torch.int64)
-        target['img_id'] = torch.as_tensor(idx, dtype=torch.int64)
+        target['image_id'] = torch.as_tensor(idx, dtype=torch.int64)
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
         target['area'] = area
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+        target['iscrowd'] = iscrowd
 
         if self.transforms is not None:
             img, target = self.transforms(img, target)
@@ -61,5 +66,6 @@ class FruitDataset(torch.utils.data.Dataset):
         return len(self.imgs)
     
 #%%
-dataset = FruitDataset('data/train')
-dataset[10]
+if __name__ == '__main__':
+    dataset = FruitDataset('data/train')
+    dataset[0]
--- train.py
+++ train.py
@@ -4,6 +4,8 @@ from PIL import Image
 import torch
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, FasterRCNN_ResNet50_FPN_Weights
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
 from dataset import FruitDataset
 from engine import train_one_epoch, evaluate
 import utils
@@ -16,6 +18,20 @@ def get_instance_segmentation(num_classes):
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
     return model
 
+def get_instance_segmentation_modified(num_classes):
+    backbone = torchvision.models.mobilenet_v2(weights='DEFAULT').features
+    backbone.out_channels = 1280
+    anchor_generator = AnchorGenerator(sizes=((32,64,128,256,512),),
+                                       aspect_ratios=((0.5,1.0,2.0),))
+    roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'],
+                                                    output_size=7,
+                                                    sampling_ratio=2)
+    model = FasterRCNN(backbone,
+                       num_classes=num_classes,
+                       rpn_anchor_generator=anchor_generator,
+                       box_roi_pool=roi_pooler)
+    return model
+
 def get_transform(train):
     transforms = []
     # converts the image, a PIL image, into a PyTorch Tensor
@@ -26,37 +42,39 @@ def get_transform(train):
         transforms.append(T.RandomHorizontalFlip(0.5))
     return T.Compose(transforms)
 
-dataset_train = FruitDataset('data/train', get_transform(train=True)) # add transform part
-dataset_test = FruitDataset('data/test', get_transform(train=False)) # add transform part
+def main():
+    dataset_train = FruitDataset('data/train', get_transform(train=True)) # add transform part
+    dataset_test = FruitDataset('data/test', get_transform(train=False)) # add transform part
 
-#%%
+    torch.manual_seed(1)
+    data_loader_train = torch.utils.data.DataLoader(dataset_train,
+                                                    batch_size=8,
+                                                    shuffle=True,
+                                                    collate_fn=utils.collate_fn)
 
-torch.manual_seed(1)
-data_loader_train = torch.utils.data.DataLoader(dataset_train,
-                                                batch_size = 8,
-                                                shuffle = True,
-                                                collate_fn=utils.collate_fn)
+    data_loader_test = torch.utils.data.DataLoader(dataset_test,
+                                                    batch_size = 8,
+                                                    shuffle = True,
+                                                    collate_fn=utils.collate_fn)
 
-data_loader_test = torch.utils.data.DataLoader(dataset_test,
-                                                batch_size = 8,
-                                                shuffle = True,
-                                                collate_fn=utils.collate_fn)
+    device = torch.device('cpu')
+    num_classes = 3
+    # model = get_instance_segmentation(num_classes)
+    model = get_instance_segmentation_modified(num_classes)
+    model.to(device)
 
-device = torch.device('cpu')
-num_classes = 3
-model = get_instance_segmentation(num_classes)
-model.to(device)
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
 
-params = [p for p in model.parameters() if p.requires_grad]
-optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+    num_epochs = 10
+    for epoch in range(num_epochs):
+        train_one_epoch(model,optimizer, data_loader_train, device, epoch, print_freq=10)
+        lr_scheduler.step()
+        evaluate(model, data_loader_test, device=device)
+        # print(f'\nFinished training epoch {epoch}\n')
+    return model
 
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
-
-num_epochs = 10
-for epoch in range(num_epochs):
-    train_one_epoch(model,optimizer, data_loader_train, device, epoch, print_freq=10)
-    lr_scheduler.step()
-    evaluate(model, data_loader_test, device=device)
-    print(f'Finished training epoch {epoch}')
-
+if __name__=='__main__':
+    main()
 #%%
